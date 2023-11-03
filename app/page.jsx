fb9afef726ecb707ad7886ff022eae2b690d2051
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-03 08:18:14 +0000

    Cleaning

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-center font-bold text-2xl'>Hello</div>
+    <div className='text-center font-bold text-2xl'>How</div>
   )
 }
 
