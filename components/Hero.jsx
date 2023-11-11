080fdd7f14a015945ecfd1979acfbd20562292ad
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 09:16:57 +0000

    Working on hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
    <div className='bg-white mt text-center pb-10'>
     <div>
-       <Image alt = "P" src={"/public/undraw_home_cinema_l7yl.svg"} width={200} height={200}/> 
+       <Image alt = "Picture of a guy sitting watching movie" src={"/public/undraw_home_cinema_l7yl.svg"} width={200} height={200}/> 
     </div>
 
    </div>
