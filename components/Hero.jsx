98d2c538d31097e2527bf714cc711f29bb6f611b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 09:15:27 +0000

    Working on hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
    <div className='bg-white mt text-center pb-10'>
     <div>
-       <Image src={"/public/undraw_home_cinema_l7yl.svg"} weight={} height={}/> 
+       <Image src={"/public/undraw_home_cinema_l7yl.svg"} weight={200} height={}/> 
     </div>
 
    </div>
