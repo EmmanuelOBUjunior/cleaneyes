f59e002f5f7bf91756563ceb074df378494096b8
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 16:15:46 +0000

    Working on movie card

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -1,11 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const MovieCard = ({movie}) => {
   return (
-    <>
-    <div>{movie.title}</div>
-    <div>{movie.id}</div>
-    </>
+   <Link href = "">
+   
+   </Link>
   )
 }
 
