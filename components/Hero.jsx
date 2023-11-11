289fc6b816cc08232d2523184d7f7e05397d01f5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 09:12:09 +0000

    Working on hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
    <div className='bg-white mt text-center pb-10'>
     <div>
-        
+       <Image src={} weight/> 
     </div>
 
    </div>
