68b3e5519e38b6939960c2e097ea877a55ee20ef
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 13:30:30 +0000

    Working on popular movies

--- components/PopularMovies.jsx
+++ components/PopularMovies.jsx
@@ -1,18 +1,6 @@
 
 const PopularMovies = () => {
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzAzOTQ4MTBhYmE5MWVmYTY1ZmJiYzUzYWE0ZGU2MCIsInN1YiI6IjY1NGY3NTE3Mjg2NmZhMTA4YzA1MmE1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rxk_3Zz_9NYzpGElTBeh8JizktBNmnXQUlXNqlwREhU'
-        }
-      };
-      
-      fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-        
+    
   return (
     <>
 
