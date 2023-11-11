cb18d95f286e80db77955d860e54f59b904cd702
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 10:04:30 +0000

    Working on contact us

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='text-center'>
-      <h1 className='text-4xl'>Contact Us</h1>
+      <h1 className='text-4xl mt-4'>Contact Us</h1>
     </div>
   )
 }
