69f7c2bce33473665a6945e727363499cd81ce48
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 16:32:37 +0000

    Working on movie card

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -5,12 +5,14 @@ import React from 'react'
 const MovieCard = ({movie}) => {
   return (
    <Link href = {`/movies/${movie.id}`}>
+    <div className='bg-white rounded-md shadow-sm cursor-pointer'>
     <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} width={700} height={800} alt = {`${movie.title} poster`} className='rounded-t-md'/>
     <div className='px-6 py-2'>
         <div className="font-bold text-xl mb-1">
             {movie.title}
         </div>
         <p className="text-gray-700 mb-1">{movie.release_date}</p>
+    </div>
     </div>
    </Link>
   )
