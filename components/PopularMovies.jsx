2b9a1de09dc0418f1c7f0db330685606f9f26cfb
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 13:34:01 +0000

    Working on popular movies

--- components/PopularMovies.jsx
+++ components/PopularMovies.jsx
@@ -2,7 +2,13 @@
 import {useEffect, useState} from 'react';
 
 const PopularMovies = () => {
+    const [movies, setMovies] = useState()
 
+    useEffect(() =>{
+        const getMovies = async () => {
+            const
+        }
+    },[])
   return (
     <>
         <div>Popular Movies Here</div>
