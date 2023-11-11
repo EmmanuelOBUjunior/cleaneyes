ae04027377176836f793736d5dee466c66e2ada7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 13:13:33 +0000

    Working on popular movies

--- components/PopularMovies.jsx
+++ components/PopularMovies.jsx
@@ -5,17 +5,18 @@ import { useEffect, useState } from "react"
 const PopularMovies = () => {
     const [movies, setMovies] = useState();
 
-    useEffect(() =>{
-        const getMovies = async () =>{
-            const query = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1')
-            const response = await query.json();
-
-            setMovies(response)
-            console.log(response)
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzAzOTQ4MTBhYmE5MWVmYTY1ZmJiYzUzYWE0ZGU2MCIsInN1YiI6IjY1NGY3NTE3Mjg2NmZhMTA4YzA1MmE1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rxk_3Zz_9NYzpGElTBeh8JizktBNmnXQUlXNqlwREhU'
         }
-
-        getMovies()
-    },[])
+      };
+      
+      fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
   return (
     <>
     {movies && movies.length && movies.map((movie) =>{
