26cd9fe9447277070c7f7cfb04702714c1dfa20e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 09:54:58 +0000

    Working on nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
         <div className='p-4 font-bold text-neutral-100 tracking-widest max-w-7xl mx-auto container'>
             <Link href= "/">
                 <h1 className='text-base md:text-2xl'>Clean
-                    <span className='text-red-'>Eyes</span>
+                    <span className='text-red-600'>Eyes</span>
                 </h1>
             </Link>
         </div>
