3e69008677b81c339f52ed32e421b7919ccc7234
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 13:09:49 +0000

    Working on popular movies

--- components/PopularMovies.jsx
+++ components/PopularMovies.jsx
@@ -1,21 +1,33 @@
 'use client'
 
-import { useEffect } from "react"
-const PopularMovies = () => {
+import { useEffect, useState } from "react"
 
+const PopularMovies = () => {
+    const [movies, setMovies] = useState();
 
     useEffect(() =>{
         const getMovies = async () =>{
             const query = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1')
             const response = await query.json();
 
+            setMovies(response)
             console.log(response)
         }
 
         getMovies()
     },[])
   return (
-    {}
+    <>
+    {movies && movies.length && movies.map((movie) =>{
+        return (
+            <>
+            <div>{movie.title}</div>
+            </>
+        )
+    })
+    
+    }
+    </>
   )
 }
 
