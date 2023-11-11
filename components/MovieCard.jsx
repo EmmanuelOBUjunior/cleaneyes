ad80b54bc5119781f4dafea0e9eddccb5c4ff3b8
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 16:25:16 +0000

    Working on movie card

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const MovieCard = ({movie}) => {
   return (
    <Link href = {`/movies/${movie.id}`}>
-    <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} width={700} height={800}/>
+    <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} width={700} height={800} alt = {`${movie.title} poster`}/>
    
    </Link>
   )
