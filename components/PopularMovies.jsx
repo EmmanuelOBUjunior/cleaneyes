ba1c20eceec875f29d41f1731b73407a02eb0113
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 13:42:52 +0000

    Working on popular movies

--- components/PopularMovies.jsx
+++ components/PopularMovies.jsx
@@ -6,7 +6,9 @@ const PopularMovies = () => {
 
     useEffect(() =>{
         const getMovies = async () => {
-            const
+            const fetchMovies = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=770394810aba91efa65fbbc53aa4de60');
+            const movieresponse = await fetchMovies.json();
+            console.log(movieresponse)
         }
     },[])
   return (
