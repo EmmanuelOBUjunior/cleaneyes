caa7e2f7a4a857b21b15ff1cd477d477f8a61f8a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 09:22:00 +0000

    Working on hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Hero = () => {
   return (
    <div className='bg-white mt text-center pb-10'>
-    <div>
+    <div className='w-60 mx-auto'>
        <Image alt = "Picture of a guy sitting watching movie" src={"/public/undraw_home_cinema_l7yl.svg"} width={200} height={200}/> 
     </div>
 
