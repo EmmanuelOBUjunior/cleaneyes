1c0807417a3b2d1fcbd39b0973734a6e7f0ae788
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 13:05:13 +0000

    Working on popular movies

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,10 @@
-import Image from "next/image"
-import Link from "next/link"
+import Hero from "@/components/Hero"
 import PopularMovies from "@/components/PopularMovies"
 
 export default function Home() {
   return (
     <section>
-    <div className='bg-white mt text-center pb-10'>
-    <div className='w-60 mx-auto'>
-       <Image alt = "Picture of a guy sitting watching movie" src={"/undraw_home_cinema_l7yl.png"} width={200} height={200}/> 
-    </div>
-    <h1 className='text-3xl text-gray-700 uppercase font-bold font-neue'>Welcome to Clean Eyes👌</h1>
-    <p className='text-gray-700'>Find our thrilling and exciting featured movies here. Trust me you will love it❤️</p>
-    <Link href="/contact">
-    <button className='bg-gray-700 text-white py-3 px-6 text-sm mt-4 rounded hover:bg-gray-500'>Contact Us</button>
-    </Link>
-   </div>
+    <Hero/>
     <PopularMovies/>
     </section>
   )
