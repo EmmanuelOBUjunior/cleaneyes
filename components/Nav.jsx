f6cb0f67d16fa3a62627d0320b92be5979a6c711
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 09:51:02 +0000

    Working on navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Nav = () => {
   return (
     <div className='bg-gray-700'>
-        <div>
+        <div className='p-4 font-bold text-neutral-100'>
             <Link href= "/">
                 <h1>Clean
                     <span>Eyes</span>
