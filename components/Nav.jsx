aebf9c2db3ce220217ec19bfb8a80ba7051ffa45
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 10:02:01 +0000

    Added google font

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
     <div className='bg-gray-700'>
         <div className='p-4 font-bold text-neutral-100 tracking-widest max-w-7xl mx-auto container'>
             <Link href= "/">
-                <h1 className='text-base md:text-2xl font-neue'>Clean
+                <h1 className='text-base md:text-2xl'>Clean
                     <span className='text-red-600'>Eyes</span>
                 </h1>
             </Link>
