13c713b868785fe2cc1fca09a5ba48309912bea0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 14:51:41 +0000

    Hiding stuff

--- components/PopularMovies.jsx
+++ components/PopularMovies.jsx
@@ -6,7 +6,7 @@ const PopularMovies = () => {
 
     useEffect(() =>{
         const getMovies = async () => {
-            const fetchMovies = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${process.env.API_KEY}`);
+            const fetchMovies = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${process.env.MY_API_KEY}`);
             const movieresponse = await fetchMovies.json();
             console.log(movieresponse.results)
             
