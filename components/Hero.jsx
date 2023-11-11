73202d5aa4b4185a06e4d17cae37e7a6aef3bbdd
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-11 09:29:05 +0000

    Working on hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
        <Image alt = "Picture of a guy sitting watching movie" src={"/undraw_home_cinema_l7yl.png"} width={200} height={200}/> 
     </div>
     <h1 className='text-3xl text-gray-700 uppercase font-bold'>Welcome to Clean Eyes👌</h1>
-    <p className='text-gray-700'></p>
+    <p className='text-gray-700'>Find our thrilling and exciting featured movies here. Trust me you will love it❤️</p>
    </div>
   )
 }
