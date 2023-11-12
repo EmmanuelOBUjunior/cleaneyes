31a8b400fe3e59cd4cd258ae067e865f8cf73f6c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-12 13:27:16 +0000

    Working on Movie Details page

--- app/movies/[id]/page.js
+++ app/movies/[id]/page.js
@@ -3,20 +3,21 @@ import {useEffect, useState} from 'react'
 import { useParams } from 'next/navigation'
 
 const MovieDetails = () => {
-  const [details, getDetails] = useState()
+  const [detail, getDetails] = useState()
   const params = useParams()
 
   useEffect(() =>{
     const detailsFetch = async () =>{
       const fetchdetail = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?language=en-US&api_key=770394810aba91efa65fbbc53aa4de60`)
       const response = await fetchdetail.json()
+      console.log(response)
       getDetails(response)
     }
 
     detailsFetch();
-  })
+  },[params.id])
   return (
-    <div>Movie Details</div>
+    <div>{details.original_title}</div>
   )
 }
 
