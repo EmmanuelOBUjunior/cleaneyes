9c771e1ce7e89e3e388e367e84655116905b4c02
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-12 17:43:17 +0000

    Working on 404 page

--- app/not-found.js
+++ app/not-found.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 const NotFound = () => {
@@ -10,7 +10,9 @@ const NotFound = () => {
         const timer = setTimeout(() => {
             router.push('./')
         }, 5000)
-    },[])
+
+        return () => clearTimeout(timer)
+    })
 
   return (
     <div>The page you are looking for cannot be found</div>
