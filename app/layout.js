e67c27e988e886428e7c2a40276481e7e1ab0e50
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-12 17:21:46 +0000

    Working on meta component

--- app/layout.js
+++ app/layout.js
@@ -3,16 +3,15 @@ import Meta from '@/components/Meta'
 import Nav from '@/components/Nav'
 import '@/styles/globals.css'
 
-// export const metadata = {
-//   title: 'Clean Eyes',
-//   description: 'Clean your eyes mahn, you deserve it',
-// }
+export const metadata = {
+  title: 'Clean Eyes',
+  description: 'Clean your eyes mahn, you deserve it',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Meta/>
         <Nav/>
         {children}
         </body>
