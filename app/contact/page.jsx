f2a03052ef116bad23565693f4dbac3ff3d8f08d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-12 17:28:17 +0000

    Working on meta component

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,10 @@
+import Meta from '@/components/Meta'
 import React from 'react'
 
 const Contact = () => {
   return (
     <div className='text-center'>
+      <Meta title = "Contact US"/>
       <h1 className='text-4xl mt-4 font-neue'>Contact Us</h1>
     </div>
   )
