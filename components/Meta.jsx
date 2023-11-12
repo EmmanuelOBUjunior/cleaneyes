ad0edc428f59a3cfb215d0fb606eceaf33226cc3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-12 17:08:28 +0000

    Working on meta component

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 const Meta = () => {
   return (
     <Head>
-        <meta name='viewport'/>
+        <meta name='viewport' content='width-device-width, initial-scale=1'/>
+        <meta name='keywords' content='width-device-width, initial-scale=1'/>
     </Head>
   )
 }
