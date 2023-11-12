cfae5115baf3f862b81c3cc2b539f3be74b06282
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-12 12:59:26 +0000

    Working on Movie Details page

--- app/movies/[id]/page.js
+++ app/movies/[id]/page.js
@@ -3,6 +3,12 @@ import {useEffect, useState} from 'react'
 
 const MovieDetails = () => {
   const [details, getDetails] = useState()
+
+  useEffect(() =>{
+    const detailsFetch = async () =>{
+      const fetchdetail = await fetch()
+    }
+  },[])
   return (
     <div>Movie Details</div>
   )
