5745c4e64a0c62b5f332e4bc71d41d95cd1b163a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-12 13:01:50 +0000

    Working on Movie Details page

--- app/movies/[id]/page.js
+++ app/movies/[id]/page.js
@@ -7,6 +7,8 @@ const MovieDetails = () => {
   useEffect(() =>{
     const detailsFetch = async () =>{
       const fetchdetail = await fetch(`https://api.themoviedb.org/3/movie/678512?language=en-US&api_key=770394810aba91efa65fbbc53aa4de60`)
+      const response = await fetchdetail.json()
+      console.log(response)
     }
   },[])
   return (
